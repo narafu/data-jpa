92ae65b85a1ddbb99f2812b3946b34e790dd80b7
phs <phs0918@imedisync.com>
2022-06-20 20:05:10 +0900

    ~ section 3-1

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,42 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+// JPA는 기본생성자를 필요로 한다. public은 너무 개방적, private은 읽지 못함. protected부터 읽음.
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 성능최적화 위해서
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,42 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Member save(Member member) {
+        entityManager.persist(member);
+        return member;
+    }
+
+    public void delete(Member member) {
+        entityManager.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = entityManager.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return entityManager.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+}
